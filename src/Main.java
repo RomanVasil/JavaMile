468fb4754aa35081689196c93e76ed70124b0464
RomanVasil <jostik85@outlook.com>
2022-04-20 15:04:32 +0400

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        long price=1010;    // стоимость билета
-        long priseMile=20;  // стоимость одной бонусной милли
+        long price = 1010;    // стоимость билета
+        long priseMile = 20;  // стоимость одной бонусной милли
 
-        long mile=price/priseMile;  // рассчет количества бонусных милль
-        System.out.println(mile +" миль(и) начисленно");  //вывод на экран
+        long mile = price / priseMile;  // рассчет количества бонусных милль
+        System.out.println(mile + " миль(и) начисленно");  //вывод на экран
     }
 }
